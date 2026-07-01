8aca325e7fd17a7c362353e2f1af9761992138bc
xuzhen97 <xuzhen97@outlook.com>
2026-07-01 12:56:42 +0800

    replace .cmd with .ps1 for Windows users

--- scripts/setup-examples.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-rem ponytail: cmd equivalent of setup-examples.sh
-cd /d "%~dp0.."
-git submodule update --init --recursive
---
+++ scripts/setup-examples.ps1
@@ -0,0 +1,3 @@
+# ponytail: one-liner wrapped for discoverability
+Set-Location (git rev-parse --show-toplevel)
+git submodule update --init --recursive
